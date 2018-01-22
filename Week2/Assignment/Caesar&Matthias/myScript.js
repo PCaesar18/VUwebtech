fecf303d32c167e93012ed444f9dd522c0b108e5
Caesar <pepijndekeijzer@gmail.com>
2018-01-22 01:10:38 +0100

    new

--- Week2/Assignment/Caesar&Matthias/myScript.js
+++ Week2/Assignment/Caesar&Matthias/myScript.js
@@ -1,4 +1,4 @@
-//this loads html content from the VU into the table
+//this loads html content from the VU into the table onload, send, reset
 $(document).ready(function()
 {
     $.ajax({
@@ -7,149 +7,61 @@ $(document).ready(function()
         type: 'get',
         dataType:'json',
         success: function (response)
-        //if succesful do the following
         {
           var trHTML = '';
-          //set up an empty var
           $.each(response, function (key,value) {
-            //for each object in the json file look at the key and the value pair
              trHTML +=
-             //concatenate the key value pairs that were passed with the correct html tag
-               '<tr><td>' + value.brand +
-               '</td><td>' + value.model +
-               '</td><td>' + value.os +
-               '</td><td><img src="' + value.image +
-               '"></td><td>' + value.screensize +
-               '</td></tr>';
+             '<tr><td>' + value.brand +
+             '</td><td>' + value.model +
+             '</td><td>' + value.os +
+             '</td><td><img src="' + value.image +
+             '"></td><td>' + value.screensize +
+             '</td></tr>';
           });
+
             $('#myTable').append(trHTML);
-            //append to table named myTable the string trHTML
         }
     });
-});
+    $('form').submit(function(event) { //Trigger on form submit
+        //Validate fields if required using jQuery
+        var postForm = $('form').serialize();
+
+        $.ajax({ //Process the form using $.ajax()
+            type      : 'POST', //Method type
+            url       : 'https://wt.ops.labs.vu.nl/api18/f47cbdfe',
+            //Your form processing file URL
+            data      : postForm, //Forms name
+            dataType:"json",
+            success: function (response)
+            {
+              $.get(response.URI, function(value){
+                var trHTML = '';
+                   trHTML +=
+                   '<tr><td>' + value.brand +
+                   '</td><td>' + value.model +
+                   '</td><td>' + value.os +
+                   '</td><td><img src="' + value.image +
+                   '"></td><td>' + value.screensize +
+                   '</td></tr>';
+                  $('#myTable').append(trHTML);
+              })
+            }
+        });
+        event.preventDefault(); //Prevent the default submit
+    });
+    $("#reset").click(function(){ //resets the database
+        $.get("https://wt.ops.labs.vu.nl/api18/f47cbdfe/reset",function(response){
 
 
-$(document).ready(function()
-{
-    $.ajax({
-      //ajax request with paramaters to get a json file from the VU
-        url: 'https://wt.ops.labs.vu.nl/api18/f47cbdfe',
-        type: 'get',
-        dataType:'json',
-        success: function (response)
-        //if succesful do the following
-        {
-          var trHTML = '';
-          //set up an empty var
-          $.each(response, function (key,value) {
-            //for each object in the json file look at the key and the value pair
-             trHTML +=
-             //concatenate the key value pairs that were passed with the correct html tag
-               '<tr><td>' + value.brand +
-               '</td><td>' + value.model +
-               '</td><td>' + value.os +
-               '</td><td><img src="' + value.image +
-               '"></td><td>' + value.screensize +
-               '</td></tr>';
-          });
-            $('#myTable').append(trHTML);
-            //append to table named myTable the string trHTML
-        }
+              alert("Your database is reset! Please reload the page");
+
+
+        });
     });
 });
-
-
-
-/*
-$(document).ready(function() {
-        var form = $("#myForm");
-
-        $("#myform").submit(function(e) {
-            e.preventDefault();
-            $.ajax({
-            url: "https://wt.ops.labs.vu.nl/api18/f47cbdfe",
-            type: "POST",
-            data: JSON.stringify(data),
-            contentType: "application/json",
-            complete: callback
-            });
-        })
-    })
-
-
-
-/*
-$('a').click(function () {
-    // custom handling here
-    return false;
-});
-
-$('submit').submit(function(event) {$.ajax({
-url: "https://wt.ops.labs.vu.nl/api18/f47cbdfe",
-type: "POST",
-data: JSON.stringify(data),
-contentType: "application/json",
-complete: callback
-});
-})
-
-
-$(document).ready(function() {
-  $("#submit").click(function(event){
-      event.preventDefault();
-      $.ajax({
-      url: "https://wt.ops.labs.vu.nl/api18/f47cbdfe",
-      type: "POST",
-      data: JSON.stringify(data),
-      contentType: "application/json",
-      complete: callback
-      });
-  });
-});
-
-
-/*
-$("#submit").on('click', function(e){
-    e.preventDefault();
-    $.ajax({
-    url: "https://wt.ops.labs.vu.nl/api18/f47cbdfe",
-    type: "POST",
-    data: JSON.stringify(data),
-    contentType: "application/json",
-    complete: callback
-    });
-});
-
-/*
-    $('#submit').click(function(e){
-      return false;
-
-
-
-      $.ajax({
-      url: "https://wt.ops.labs.vu.nl/api18/f47cbdfe",
-      type: "POST",
-      data: JSON.stringify(data),
-      contentType: "application/json",
-      complete: callback
-      });
-      /*
-      $.ajax({
-        type: 'post',
-        url: "https://wt.ops.labs.vu.nl/api18/f47cbdfe",
-        dataType: 'json',
-        succes  : function(data){
-          alert("You succesfully submitted data");
-          $("#testresult").html(data);
-        }});
-        */
-
-  //  });
-
 //this was sourced from stackoverflow https://stackoverflow.com/questions/31074532/using-jquery-to-build-table-rows-from-ajax-response-not-with-static-json-data
 
-
-
+//Sorting
 function sortTable(n) {
   var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
   table = document.getElementById("myTable");
@@ -204,14 +116,3 @@ function sortTable(n) {
     }
   }
 }//this code was sourced from https://www.w3schools.com/howto/howto_js_sort_table.asp
-
-//reset function
-
-$(document).ready(function(){
-    $("#reset").click(function(event){
-      event.preventDefault(); //this prevents the submit button to redirect us to the database page
-        $.get("https://wt.ops.labs.vu.nl/api18/f47cbdfe/reset", function(){
-            alert("Your database is reset!"); //a small alert to warn the user that the database is reset
-        });
-    });
-});
--- Week2/Assignment/Caesar/myScript.js
+++ Week2/Assignment/Caesar/myScript.js
@@ -1,7 +1,6 @@
 //this loads html content from the VU into the table
 $(document).ready(function()
 {
-
     $.ajax({
       //ajax request with paramaters to get a json file from the VU
         url: 'https://wt.ops.labs.vu.nl/api18/f47cbdfe',
@@ -28,6 +27,125 @@ $(document).ready(function()
         }
     });
 });
+
+
+$(document).ready(function()
+{
+    $.ajax({
+      //ajax request with paramaters to get a json file from the VU
+        url: 'https://wt.ops.labs.vu.nl/api18/f47cbdfe',
+        type: 'get',
+        dataType:'json',
+        success: function (response)
+        //if succesful do the following
+        {
+          var trHTML = '';
+          //set up an empty var
+          $.each(response, function (key,value) {
+            //for each object in the json file look at the key and the value pair
+             trHTML +=
+             //concatenate the key value pairs that were passed with the correct html tag
+               '<tr><td>' + value.brand +
+               '</td><td>' + value.model +
+               '</td><td>' + value.os +
+               '</td><td><img src="' + value.image +
+               '"></td><td>' + value.screensize +
+               '</td></tr>';
+          });
+            $('#myTable').append(trHTML);
+            //append to table named myTable the string trHTML
+        }
+    });
+});
+
+
+
+/*
+$(document).ready(function() {
+        var form = $("#myForm");
+
+        $("#myform").submit(function(e) {
+            e.preventDefault();
+            $.ajax({
+            url: "https://wt.ops.labs.vu.nl/api18/f47cbdfe",
+            type: "POST",
+            data: JSON.stringify(data),
+            contentType: "application/json",
+            complete: callback
+            });
+        })
+    })
+
+
+
+/*
+$('a').click(function () {
+    // custom handling here
+    return false;
+});
+
+$('submit').submit(function(event) {$.ajax({
+url: "https://wt.ops.labs.vu.nl/api18/f47cbdfe",
+type: "POST",
+data: JSON.stringify(data),
+contentType: "application/json",
+complete: callback
+});
+})
+
+
+$(document).ready(function() {
+  $("#submit").click(function(event){
+      event.preventDefault();
+      $.ajax({
+      url: "https://wt.ops.labs.vu.nl/api18/f47cbdfe",
+      type: "POST",
+      data: JSON.stringify(data),
+      contentType: "application/json",
+      complete: callback
+      });
+  });
+});
+
+
+/*
+$("#submit").on('click', function(e){
+    e.preventDefault();
+    $.ajax({
+    url: "https://wt.ops.labs.vu.nl/api18/f47cbdfe",
+    type: "POST",
+    data: JSON.stringify(data),
+    contentType: "application/json",
+    complete: callback
+    });
+});
+
+/*
+    $('#submit').click(function(e){
+      return false;
+
+
+
+      $.ajax({
+      url: "https://wt.ops.labs.vu.nl/api18/f47cbdfe",
+      type: "POST",
+      data: JSON.stringify(data),
+      contentType: "application/json",
+      complete: callback
+      });
+      /*
+      $.ajax({
+        type: 'post',
+        url: "https://wt.ops.labs.vu.nl/api18/f47cbdfe",
+        dataType: 'json',
+        succes  : function(data){
+          alert("You succesfully submitted data");
+          $("#testresult").html(data);
+        }});
+        */
+
+  //  });
+
 //this was sourced from stackoverflow https://stackoverflow.com/questions/31074532/using-jquery-to-build-table-rows-from-ajax-response-not-with-static-json-data
 
 
@@ -87,27 +205,13 @@ function sortTable(n) {
   }
 }//this code was sourced from https://www.w3schools.com/howto/howto_js_sort_table.asp
 
-
-
-
-
-
-$(document).ready(function()
-{
-  $('#reset').click(function(){
-    $.post("https://wt.ops.labs.vu.nl/api18/f47cbdfe", JSON.stringify(data))}
-  );
-});
-
-
-
-
-
 //reset function
+
 $(document).ready(function(){
-    $("#reset").click(function(){
+    $("#reset").click(function(event){
+      event.preventDefault(); //this prevents the submit button to redirect us to the database page
         $.get("https://wt.ops.labs.vu.nl/api18/f47cbdfe/reset", function(){
-            alert("Your database is reset!");
+            alert("Your database is reset!"); //a small alert to warn the user that the database is reset
         });
     });
 });
